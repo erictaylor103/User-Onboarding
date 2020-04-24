55c9223d1c00b8b139233ad4a56c7d2ecc3ea227
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-04-23 22:29:01 -0400

    Met MVP

---
+++ src/UserData.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+const UserData = props => {
+    
+    
+    return(
+        <div>
+            {props.users.map(user =>(
+                <div key={user.id}>
+                    <h2>{user.name}</h2>
+                    <p>{user.role}</p>
+                    <p>{user.email}</p>
+                    <p>{user.civil}</p>
+                </div>
+            ))}
+        </div>    
+    )
+};
+
+export default UserData;
